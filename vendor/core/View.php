08d74eff57e1670aa8d973b4d068ce79f6137072
user <user@user.com>
2018-12-14 17:10:47 +0200

    page

--- vendor/core/View.php
+++ vendor/core/View.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace core;
+
 class View {
     
     public $template='main';
-    
+    public $page;
+
+
     public function render(){
 	include_once 'vender/views/templates/'.$this->template.'.php';
     }
