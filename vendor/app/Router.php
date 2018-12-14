1d9010767aedcae7e76106282506a5e9c84c1978
user <user@user.com>
2018-12-14 16:45:30 +0200

    namespaces

--- vendor/app/Router.php
+++ vendor/app/Router.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace app;
 
-/**
- * Description of router
- *
- * @author web
- */
 class Router {
-    //put your code here
+
+    static public function init() {
+	
+    }
+
 }
